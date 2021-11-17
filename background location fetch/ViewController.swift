64be814f9db9c1f86b39bed0c98ab2467520fb2d
Nguyen Viet My <zzvietmyzz@gmail.com>
2021-11-17 11:19:48 +0700

    Initial Commit

---
+++ background location fetch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  background location fetch
+//
+//  Created by VinBrain on 17/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
